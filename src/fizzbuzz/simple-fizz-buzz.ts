0ee3492a14c3108a0ce39bcda60143dfaac3aeab
AlexAegis <alexaegis@gmail.com>
2020-10-15 23:01:18 +0200

    feat: fizzbuzz

---
+++ src/fizzbuzz/simple-fizz-buzz.ts
@@ -0,0 +1,18 @@
+/**
+ * Simple solution
+ */
+export const fizzBuzz = (until: number = 100): void => {
+	const toFizzBuzz = (i: number): number | string => {
+		const t = i % 3 === 0;
+		const f = i % 5 === 0;
+		if (t && f) return 'FizzBuzz';
+		else if (t) return 'Fizz';
+		else if (f) return 'Buzz';
+		else return i;
+	};
+	for (let i = 0; i <= until; i++) {
+		console.log(toFizzBuzz(i));
+	}
+};
+
+fizzBuzz();
